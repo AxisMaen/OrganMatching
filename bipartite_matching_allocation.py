e8296def682f59d79cd78b01e3887a09b67f3400
Vivdaddy <vja5074@psu.edu>
2020-09-14 09:49:37 -0400

    Fixed Gekko issue returning bad allocation by removing redundant equations and focusing only on one set. Should work now. Need to test extensively though. Maybe add command line parsing of nodes in set a, nodes in set b and edges to make it more accessible.

--- bipartite_matching_allocation.py
+++ bipartite_matching_allocation.py
@@ -1,40 +1,39 @@
-import os
 from gekko import GEKKO
 import networkx as nx
-
-
-def main():
-    print("Vivek")
-    return
 
 
 def maximum_bipartite_matching_optimization(G):
     """
     Performs LP optimization solving the maximum bipartite matching problem
-    :param G: Bipartite Networkx graph G
-    :return: dictionary of matching where key is node, value is node in other set
-    """
+    :param G: Bipartite Networkx graph G, need to specify which set each nodes by additional option
+            EG: G = nx.Graph()
+                # People
+                G.add_nodes_from(['p' + str(i) for i in range(4)], bipartite=0) # Specifying that nodes in set 0
+                # Organs
+                G.add_nodes_from(['o' + str(i) for i in range(4)], bipartite=1) # Specifying that nodes in set 1
+                # Edges
+                G.add_edges_from([('p0', 'o0'), ('p1', 'o1'), ('p2', 'o1'), ('p2', 'o2')])
 
+    :return: dictionary of matching where key is node, value is node in other set, it contains (a,b) and (b,a)
+    """
+    # remove nodes with no edges as it messes up bipartite splitter
+    left_set = [n for n in G.nodes if G.nodes[n]['bipartite'] == 0]
     model = GEKKO()
     variable_dict = {}
-    intermediates = [None] * len(G.nodes)
-    for node in list(G.nodes):
+    for node in left_set:
         neighbors = list(G.neighbors(node))
-        print("Node is ", node)
-        print("Neighbors are ", neighbors)
+        # print("Node is ", node)
+        # print("Neighbors are ", neighbors)
         for neighbor in neighbors:
             # make variables
             # lower bound 0 upper bound 1
             variable_dict[(node, neighbor)] = model.Var(lb=0, ub=1, integer=True)
 
         tuples = [(node, i) for i in neighbors]
-
+        # print(tuples)
         # Constraint that no node can be in matching twice
-        model.Equation(sum([variable_dict[tup] for tup in tuples]) <= 1)
-
-    for variable in variable_dict:
-        print(variable, tuple(reversed(variable)))
-        model.Equation(variable_dict[variable] == variable_dict[tuple(reversed(variable))])
+        if len(tuples) > 0:
+            model.Equation(sum([variable_dict[tup] for tup in tuples]) <= 1.0)
 
     # Objective
     model.Obj(-1 * sum([variable_dict[variable] for variable in variable_dict]))
@@ -43,7 +42,7 @@ def maximum_bipartite_matching_optimization(G):
     model.options.SOLVER = 1
 
     # Solve
-    model.solve()
+    model.solve(disp=True)
 
     matching = {}
     # Add variables to matching
@@ -51,13 +50,19 @@ def maximum_bipartite_matching_optimization(G):
         print(variable)
         if int(variable[1][0]) == 1:
             matching[variable[0][0]] = variable[0][1]
+            matching[variable[0][1]] = variable[0][0]
     return matching
 
 
 if __name__ == "__main__":
     G = nx.Graph()
-    G.add_edges_from([(0, 3), (0, 6), (1, 3), (1, 4), (2, 4), (2, 5)])
+    # People
+    G.add_nodes_from(['p' + str(i) for i in range(4)], bipartite=0)
+    # Organs
+    G.add_nodes_from(['o' + str(i) for i in range(4)], bipartite=1)
+    # Edges
+    G.add_edges_from([('p0', 'o0'), ('p1', 'o1'), ('p2', 'o1'), ('p2', 'o2')])
     matching_opt = maximum_bipartite_matching_optimization(G)
     print(matching_opt)
-    matching_hk = nx.bipartite.hopcroft_karp_matching(G)
+    matching_hk = nx.bipartite.hopcroft_karp_matching(G, top_nodes=[n for n in G.nodes if G.nodes[n]['bipartite'] == 0])
     print(matching_hk)
