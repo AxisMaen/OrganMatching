d773d72d5a42e64db7f018304375e106c37e2c27
Matthew Rodgers <mrr17matt@gmail.com>
2020-10-22 08:30:29 -0400

    now takes one source coord and list of dest coords, returns list of times

--- bipartite_mapbox.py
+++ bipartite_mapbox.py
@@ -1,26 +1,33 @@
 import requests
 
-#https://docs.mapbox.com/api/navigation/#matrix
+#coords are strings in the form latitude,longtitude (no spaces)
+#takes one source coord and a list of dest coords
+#returns a list of times from source coo
 
-#inputs are strings in the form latitude,longtitude (no spaces)
-def doRequest(source_coord, dest_coord):
+#distance between source and dest cannot exceed 10000km
+def doRequest(source_coord, dest_coords):
     
     base_path = "https://api.mapbox.com"
     endpoint = "/directions/v5/"
     profile = "mapbox/driving-traffic/"
-    #coords = "-122.42,37.78;-122.45,37.91"
-    coords = source_coord + ";" + dest_coord
+    #coords = "-122.42,37.78;-122.45,37.91"  
     access_token = "access_token=pk.eyJ1IjoiYXhpczE3IiwiYSI6ImNrZnh5eHd5cDI2ZHAycW84bncya3A4YncifQ.wDVnZ5-pJ6iAayFHDrOUCQ"
+    times = []
 
-    request_url = base_path + endpoint + profile + coords + "?" + access_token
 
-    response = requests.get(request_url)
-    json_data = response.json()
+    for dest_coord in dest_coords:
+        coords = source_coord + ";" + dest_coord
+        request_url = base_path + endpoint + profile + coords + "?" + access_token
+        
+        response = requests.get(request_url)
+        json_data = response.json()
+        
+        if(json_data["code"] != "Ok"):            
+            raise Exception(json_data["message"])
+        
+        times.append(json_data["routes"][0]["duration"]) #most optimal time
 
-    if(json_data["code"] != "Ok"):
-        raise Exception("Bad response from Mapbox")
+    return times
 
-    #see https://docs.mapbox.com/api/navigation/#response-retrieve-a-matrix for the response format
-    
-    print(json_data["routes"][0]["duration"]) #most optimal time
-    return json_data
+#dests = ["-122.45,37.91", "-122.45,37.91", "-122.45,37.91"]
+#print(doRequest("-122.42,37.78", dests))
